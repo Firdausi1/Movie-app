3cfd3d4aa9b5344d05b5266acb25021f438677d2
Firdausy Bashir <43407297+Firdausi1@users.noreply.github.com>
2020-04-25 11:54:16 +0100

    comment section added

---
+++ models/movie.js
@@ -0,0 +1,19 @@
+var mongoose = require("mongoose");
+
+var movieSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    comments: [
+        {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "comment"
+        }
+    ],
+    cast: String,
+    director: String,
+    Date: {type:Date, default: Date.now}
+})
+
+
+module.exports = mongoose.model("movie", movieSchema) 
